fdbbb7b940925a5cabeb67a96985f45810351081
CarloVeronese <carlo.veronese97@gmail.com>
2023-10-03 13:46:55 +0200

    Add - three prompt in js

---
+++ README.md
@@ -0,0 +1,18 @@
+# Salutare l'utente
+
+- chiedere all'utente il suo nome
+    - dichiarare una variabile
+    - assegnare ad una variabile il valore restituito da un prompt
+- chiedere all'utente il suo cognome
+    - dichiarare una variabile
+    - assegnare ad una variabile il valore restituito da un prompt
+- chiedere all'utente il suo colore preferito
+    - dichiarare una variabile
+    - assegnare ad una variabile il valore restituito da un prompt
+- concatenare i dati dell'utente con un numero generato randomicamente
+    - dichiarare una variabile che contiene il numero da inserire nella password
+    - dichiarare una variabile password dove concateniamo le quattro stringhe
+- stampare un messaggio dentro un h1
+    - inserire nel html un tag g1 con id='password'
+    - recuperare con js l'elemento del dom con id='password'
+    - modificare l'innerHTML del h1 con il valore della variabile password
---
+++ js/app.js
@@ -0,0 +1,25 @@
+// - chiedere all'utente il suo nome
+//     - dichiarare una variabile
+let firtsName
+//     - assegnare ad una variabile il valore restituito da un prompt
+firtsName = prompt('Qual è il tuo nome?')
+console.log(firtsName);
+// - chiedere all'utente il suo cognome
+//     - dichiarare una variabile
+let lastName
+//     - assegnare ad una variabile il valore restituito da un prompt
+lastName = prompt('Qual è il tuo cognnome?')
+console.log(lastName);
+// - chiedere all'utente il suo colore preferito
+//     - dichiarare una variabile
+let favouriteColor
+//     - assegnare ad una variabile il valore restituito da un prompt
+favouriteColor = prompt('Qual è il tuo colore preferito?')
+console.log(favouriteColor);
+// - concatenare i dati dell'utente con un numero generato randomicamente
+//     - dichiarare una variabile che contiene il numero da inserire nella password
+//     - dichiarare una variabile password dove concateniamo le quattro stringhe
+// - stampare un messaggio dentro un h1
+//     - inserire nel html un tag g1 con id='password'
+//     - recuperare con js l'elemento del dom con id='password'
+//     - modificare l'innerHTML del h1 con il valore della variabile password
